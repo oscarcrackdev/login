01ba5240438edbbebb6c013df2be83e51fdeb79c
Kenzo douchet <kenzodouchet33@gmail.com>
2025-08-04 16:47:12 +0200

    create index.php with simple doc

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
-echo "hello";
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login And Register</title>
+  </head>
+  <body>
+    <!-- ma partie -->
+    <header>
+
+    </header>
+    <!-- Ta partie oscar -->
+    <main>
+
+    </main>
+  </body>
+</html>
