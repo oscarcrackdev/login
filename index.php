53b4926b43df7dee62790d9b062442fac6ef4f03
Kenzo douchet <kenzodouchet33@gmail.com>
2025-08-04 17:01:46 +0200

    add login form in header of index.php

--- index.php
+++ index.php
@@ -6,8 +6,42 @@
   </head>
   <body>
     <!-- ma partie -->
-    <header>
+     <style>
+      * {
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+      }
+      header {
+        display: flex;
+        justify-content: space-between;
+        background-color: blue;
+        padding: 20px;
+        color: white;
+      }
+      .form-control{
+        border: none;
+        padding: 10px;
+        font-size: 15px;
+        outline: none;
+      }
+      .btn-sub {
+        padding: 10px;
+        background-color: green;
+        border: none;
+        color: white;
+        cursor: pointer;
+        font-size: 15px;
 
+      }
+     </style>
+    <header>
+      <h1>Login</h1>
+      <form action="">
+        <input type="text" class="form-control" placeholder="Username">
+        <input type="password" class="form-control" placeholder="Password">
+        <input type="submit" value="Envoyer" class="btn-sub">
+      </form>
     </header>
     <!-- Ta partie oscar -->
     <main>
